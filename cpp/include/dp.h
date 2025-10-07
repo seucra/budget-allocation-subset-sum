ffc058b5ae5bc086da86344e09a53af20e095321
Seucra <tabrezahmed.sta33@gmail.com>
2025-10-08 00:20:03 +0530

    Finalized moving all opti files into opti/ directory

--- cpp/include/dp.h
+++
@@ -1,8 +0,0 @@
-#ifndef DP_H
-#define DP_H
-
-#include "budget_lib.h"
-
-Result run_dp(const std::vector<int>& costs, int budget);
-
-#endif // DP_H
--- cpp/include/hybrid.h
+++
@@ -1,8 +0,0 @@
-#ifndef HYBRID_H
-#define HYBRID_H
-
-#include "budget_lib.h"
-
-Result run_hybrid(const std::vector<int>& costs, int budget);
-
-#endif // HYBRID_H
